54d3c53d541f6a9d82c215468a7dd9e65b741cd0
Şevki <sevkialtindas2666@gmail.com>
2024-02-25 17:39:11 +0100

    guess project ıs done

--- game.js
+++ game.js
@@ -1,48 +1,107 @@
+//?PC'ye 1-20 arasında bir sayı tutturduk
 let rastgeleSayi = Math.ceil(Math.random() * 20);
 console.log(rastgeleSayi);
 
 let mesaj = document.querySelector(".msg");
 
 let skor = 10;
-let enYüksekSkor = 0;
+//skoru index.html'den çekebilirdik. Ama çok kullnacağımız için bu daha tercih edilen bir yol.
+
+//! Local Storage da top-score adıyla bir değişken varsa onu getir yoksa 0 olsun
+let enYuksekSkor = localStorage.getItem("top-score") || 0;
+
+//! -----browserda, DOM da top score değerinilocal storage dan okuyarak güncelle, özellikle 2. ve 3. oyuncular için gerekli
+document.querySelector(".top-score").textContent = enYuksekSkor;
 
 document.querySelector(".check").addEventListener("click", () => {
-  let tahmin = document.querySelector(".guess").value;
-  // console.log(tahmin);
+  const tahmin = document.querySelector(".guess").value;
+  // console.log(tahmin)
+  // document.querySelector(".guess").value = "";
 
-  //! tahmin girilmeden checke basınca
+  //!Tahmin girmeden butona basıldıysa
   if (!tahmin) {
-    mesaj.textContent = "Lütfen geçerli bir sayı giriniz !!";
+    mesaj.textContent = "Lütfen bir sayı giriniz...";
+
+    //!Tahminim Doğruysa
   } else if (tahmin == rastgeleSayi) {
-    mesaj.textContent = "Tebrikler Bildiniz 🙌👌";
-    document.querySelector(".check").disabled = true;
+    mesaj.textContent = "Tebrikler Bildiniz...👏";
     document.querySelector("body").style.backgroundColor = "green";
     document.querySelector(".number").textContent = rastgeleSayi;
+    document.querySelector(".check").disabled = true;
 
-    //! top score kontrolu
+    // top-score Kontrolü yap
+
+    if (skor > enYuksekSkor) {
+      localStorage.setItem("top-score", skor);
+      enYuksekSkor = skor;
+      document.querySelector(".top-score").textContent = skor;
+    }
+    //!Tahmin Yanlışsa
   } else {
-    //! tahmın yanlıssa
-    //! skor 1 den buyuk oldugu surece hakkım olsun
+    //? Skor 1'den büyük olduğu sürece hakkım var
+
     if (skor > 1) {
       skor--;
       document.querySelector(".score").textContent = skor;
-
       tahmin < rastgeleSayi
-        ? (mesaj.textContent = "Arttır ")
-        : (mesaj.textContent = "Azalt");
+        ? (mesaj.textContent = "Arttır👆")
+        : (mesaj.textContent = "Azalt👇");
     } else {
-      mesaj.textContent = "Game Over😢";
+      //! GAME OVER
+      mesaj.textContent = "Oyunu Kaybettiniz🥺";
       document.querySelector(".score").textContent = 0;
       document.querySelector("body").style.backgroundColor = "red";
     }
   }
 });
 
-document.querySelector(".again").addEventListener("click", () => {
-  document.querySelector(".check").disabled = false;
+//?Again butonuna basınca ayarlar başlangıç değerlerine kurulsun. Aka plan #2d3436 olsun
+
+document.querySelector(".again").onclick = () => {
   document.querySelector("body").style.backgroundColor = "#2d3436";
-  document.querySelector(".number").textContent = "?";
-  document.querySelector(".score").textContent = 10;
+
   rastgeleSayi = Math.ceil(Math.random() * 20);
-  console.log("yenı rastgele sayı ", rastgeleSayi);
+  console.log("yeni rastgele sayı", rastgeleSayi);
+
+  skor = 10;
+
+  document.querySelector(".score").textContent = skor;
+
+  document.querySelector(".number").textContent = "?";
+
+  document.querySelector(".guess").value = "";
+
+  mesaj.textContent = "Oyun yeni oyuncu için başlıyor... ";
+
+  document.querySelector(".check").disabled = false;
+};
+
+//! ENTER tuşu çalışsın!
+
+//Klavyeden enter butonuna basıldığında check butonunu tetikle
+
+document.addEventListener("keydown", function (e) {
+  // console.log(e)
+
+  if (e.key === "Enter") {
+    document.querySelector(".check").click();
+  }
+});
+
+//!Geçersiz sayı girdiniz 1 ile 20 arasında bir sayı giriniz..
+
+document.querySelector(".check").addEventListener("click", () => {
+  tahmin = document.querySelector(".guess").value;
+
+  const tahminSayi = parseInt(tahmin);
+
+  if (tahminSayi >= 1 && tahminSayi <= 20 && !isNaN(tahminSayi)) {
+    //Doğru sayı girilmişse oyunu devam ettir
+  } else {
+    mesaj.textContent =
+      "Geçersiz sayı girdiniz (1 - 20 arasında bir sayı giriniz";
+    skor++;
+    document.querySelector(".guess").value = "";
+    document.querySelector("body").style.backgroundColor = "magenta";
+  }
 });
